0f39a2a570a8f7da1e85464b870bdfa62b789b68
Timothy Mahajan <tmahajan@iu.edu>
2019-03-17 20:15:58 -0400

    Create guessinggame.sh

---
+++ The Unix Workbench/Week 4/guessinggame.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+function guessinggame {
+	echo "Welcome to the guessing game"
+	echo "How many files do you think are in the current directory?"
+	let local actual_count=$(ls | wc -l)
+	let local guess=$actual_count-1
+	while [[ $guess -ne $actual_count ]]
+	do
+		echo "Please enter your guess, followed by [ENTER]"
+		read your_guess
+		let guess=$your_guess
+		if [[ $guess -eq $actual_count ]]
+		then
+			echo "Congratulations, you are correct!"
+		elif [[ $guess -gt $actual_count ]]
+		then
+			echo "Sorry, your guess is too high. Please guess again!"
+		else
+			echo "Sorry, your guess is too low. Please guess again!"
+		fi
+	done
+}
